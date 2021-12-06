33d4e213e2b06689b6b2afd5ad6433655d0f96be
shahed <shahed.m@yumzy.in>
2021-12-06 17:49:28 +0530

    asfdaf

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.listen(PORT, () => {
   );
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
+// app.get("/", (req, res) => {
+//   // res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// });
 
 app.use("/todo", ToDo);
